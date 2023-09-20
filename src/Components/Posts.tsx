412e22a1b02c5ccdf7e884ccc4d489f7f7a05dbc
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-09-20 14:09:42 +0200

    feat(Components): :sparkles: Render logic
    Renders posts conditionally when entering the url named after them

---
+++ src/Components/Posts.tsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+
+import { useBlogStore } from "../data/blogpostStore";
+
+export function Posts() {
+  const blogs = useBlogStore((state) => state.blogposts);
+  const { blogpost } = useParams();
+  return (
+    <div>
+      {blogs.map((postData) => {
+        const { title, img, headline, paragraph1, paragraph2, paragraph3 } =
+          postData;
+        return (
+          <div key={title}>
+            {blogpost === title && (
+              <div>
+                <img src={img} alt={headline} />
+                <h1>{headline}</h1>
+                <p>{paragraph1}</p>
+                <p>{paragraph2}</p>
+                <p>{paragraph3}</p>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
