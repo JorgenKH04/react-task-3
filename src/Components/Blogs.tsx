5d4ab1854ab53bd9849695f44c95451db4584bf8
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-09-20 17:19:07 +0200

    style(Design): :lipstick: Css, jsx
    Small content (tsx) change, and added css

--- src/Components/Blogs.tsx
+++ src/Components/Blogs.tsx
@@ -1,20 +1,21 @@
 import { Link } from "react-router-dom";
 
 import { useBlogStore } from "../data/blogpostStore";
+import styles from "../css/Blogs.module.css";
 
 export function Blogs() {
   const blogs = useBlogStore((state) => state.blogposts);
   return (
-    <div>
+    <div className={styles.blogs}>
       {blogs.map((postData) => {
         const { title, img, headline } = postData;
         return (
-          <div>
+          <div className={styles.blogs_card}>
             <img src={img} alt={headline} />
-            <h3>{headline}</h3>
             <Link key={`${title}Link`} to={`${title}`}>
-              {title}
+              Read more
             </Link>
+            <h3>{headline}</h3>
           </div>
         );
       })}
